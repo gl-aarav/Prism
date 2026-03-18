d4c52259eb0dcbb9ed1ae4e1046280f6a9a2ef8a
Aarav Goyal <gl.aarav@gmail.com>
2026-03-17 17:04:05 -0700

    some ui changes

--- Sources/Prism/QuickToolsManager.swift
+++ Sources/Prism/QuickToolsManager.swift
@@ -19,7 +19,6 @@ class QuickToolsManager: ObservableObject {
         panel.level = .floating
         panel.collectionBehavior = [.canJoinAllSpaces, .fullScreenAuxiliary]
         panel.backgroundColor = .clear
-        panel.isOpaque = false
         panel.hasShadow = false
         panel.isReleasedWhenClosed = false
         panel.isMovableByWindowBackground = true
